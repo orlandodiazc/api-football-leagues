fcb0b16c20958d28ec03aaa2b39107cf8bf28b11
ditod1 <orlandodiazconde@gmail.com>
2023-02-17 15:08:20 -0500

    Move api functions to new file

---
+++ src/js/apiManager.js
@@ -0,0 +1,30 @@
+const myHeaders = new Headers();
+myHeaders.append('x-rapidapi-key', '11588e4db2b259eb583e66f97d257ac6');
+myHeaders.append('x-rapidapi-host', 'v3.football.api-sports.io');
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow',
+};
+
+const fetchTable = async (code) => {
+  const url = `https://v3.football.api-sports.io/standings?league=${code}&season=2022`;
+  const response = await fetch(url, requestOptions);
+  const data = await response.json();
+  return data.response[0].league;
+};
+
+const fetchLeagues = async (leagueCodes) => {
+  const result = await Promise.all(
+    leagueCodes.map(async (code) => {
+      const url = `https://v3.football.api-sports.io/leagues?id=${code}`;
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      return data.response[0];
+    }),
+  );
+  return result;
+};
+
+export { fetchLeagues, fetchTable };
