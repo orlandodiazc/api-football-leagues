2fffbbc3bc9669e5c323f9680fd13a93be9389fe
ditod1 <orlandodiazconde@gmail.com>
2023-02-17 11:01:59 -0500

    Add table local storage and fetch functionality

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,13 @@ const cardTemplate = document.getElementById('card-template');
 const modalTemplate = document.getElementById('modal-template');
 const rowTemplate = document.getElementById('table-row-template');
 
+const leagueCodes = ['39', '78'];
+const leagueList = {};
+const standingsList = {};
+
 const myHeaders = new Headers();
-myHeaders.append('X-Auth-Token', '7e1e415b0f674a85bcf4f6af1067aded');
+myHeaders.append('x-rapidapi-key', '11588e4db2b259eb583e66f97d257ac6');
+myHeaders.append('x-rapidapi-host', 'v3.football.api-sports.io');
 
 const requestOptions = {
   method: 'GET',
@@ -18,65 +23,20 @@ const requestOptions = {
 const addCardToDOM = (currentCompetition) => {
   const cardClone = cardTemplate.content.cloneNode(true);
   cardClone.querySelector('.card-title').innerText = currentCompetition.name;
-  cardClone.querySelector('img').src = currentCompetition.emblem;
-  cardClone.querySelector('[data-bs-target]').dataset.bsTarget = `#${currentCompetition.code}`;
+  cardClone.querySelector('img').src = currentCompetition.logo;
+  cardClone.querySelector('[data-bs-target]').dataset.bsTarget = `#league${currentCompetition.id}`;
   cardContainer.appendChild(cardClone);
 };
 
-const leagueCodes = ['PL', 'BL1'];
-// , 'SA', 'PD', 'FL1'
-
-const leagueList = {};
-const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-const delay = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
-
-const loadStandings = async () => {
-  const standingsList = {};
-  leagueCodes.forEach(async (code, i) => {
-    const url = `${corsProxy}http://api.football-data.org/v4/competitions/${code}/standings`;
-    const response = await fetch(url, requestOptions);
-    const data = await response.json();
-    standingsList[code] = data;
-    localStorage.setItem('leagueStandings', JSON.stringify(standingsList));
-    await delay(10 * i);
-  });
-};
-
-const loadLeagues = async () => {
-  const url = `${corsProxy}http://api.football-data.org/v4/competitions`;
-  const response = await fetch(url, requestOptions);
-  console.log(...response.headers);
-  const data = await response.json();
-  leagueCodes.forEach((code) => {
-    const currentComp = data.competitions.find((comp) => comp.code === code);
-    leagueList[code] = currentComp;
-    addCardToDOM(currentComp);
-  });
-  localStorage.setItem('leagues', JSON.stringify(leagueList));
-  await delay(10);
-};
-
-const loadLocalLeagues = () => {
-  const data = JSON.parse(localStorage.getItem('leagues'));
-  Object.keys(data).forEach((code) => {
-    addCardToDOM(data[code]);
-  });
-};
-
 const addModalToDom = (code, name, table) => {
-  const modalClone = modalTemplate.content.cloneNode(true);
-  const main = document.querySelector('main');
-  const colAttributes = [];
-
-  table.forEach((el) => {
+  const modal = document.querySelector(`#league${code}`);
+  table.forEach((row) => {
     const rowClone = rowTemplate.content.cloneNode(true);
-    rowClone.querySelector('.pos').innerText = el.position;
-    rowClone.querySelector('.team').innerText = el.team.shortName;
-    rowClone.querySelector('.games').innerText = el.playedGames;
+    rowClone.querySelector('.pos').innerText = row.rank;
+    rowClone.querySelector('.team').innerText = row.team.name;
+    rowClone.querySelector('.games').innerText = row.all.played;
     const form = rowClone.querySelector('.form');
-    // <span class="form badge text-bg-success"></span>
-    el.form.split(',').forEach((tag) => {
+    row.form.split('').forEach((tag) => {
       const span = document.createElement('span');
       span.classList.add('badge');
       span.classList.add('me-1');
@@ -90,49 +50,89 @@ const addModalToDom = (code, name, table) => {
       }
       form.appendChild(span);
     });
-    rowClone.querySelector('.won').innerText = el.won;
-    rowClone.querySelector('.draw').innerText = el.draw;
-    rowClone.querySelector('.lost').innerText = el.lost;
-    rowClone.querySelector('.pts').innerText = el.points;
-    rowClone.querySelector('.goal-for').innerText = el.goalsFor;
-    rowClone.querySelector('.goal-against').innerText = el.goalsAgainst;
-    rowClone.querySelector('.goal-dif').innerText = el.goalDifference;
-    modalClone.querySelector('.table-body').appendChild(rowClone);
+    rowClone.querySelector('.won').innerText = row.all.win;
+    rowClone.querySelector('.draw').innerText = row.all.draw;
+    rowClone.querySelector('.lost').innerText = row.all.lose;
+    rowClone.querySelector('.pts').innerText = row.points;
+    rowClone.querySelector('.goal-for').innerText = row.all.goals.for;
+    rowClone.querySelector('.goal-against').innerText = row.all.goals.against;
+    rowClone.querySelector('.goal-dif').innerText = row.goalsDiff;
+
+    modal.querySelector('.table-body').appendChild(rowClone);
   });
 
-  modalClone.querySelector('.modal-title').innerText = name;
-  modalClone.querySelector('.modal').setAttribute('id', code);
-
-  main.appendChild(modalClone);
-};
-const loadTableModal = () => {
-  const data = JSON.parse(localStorage.getItem('leagueStandings'));
-  console.log(data);
-  Object.keys(data).forEach((code) => {
-    const competitionName = data[code].competition.name;
-    const standings = data[code].standings[0].table;
-    addModalToDom(code, competitionName, standings);
-  });
+  modal.querySelector('.modal-title').innerText = name;
 };
 
-if (localStorage.getItem('leagueStandings')) {
-  loadTableModal();
-}
-
-const loadScorers = async () => {
-  const url = `${corsProxy}http://api.football-data.org/2006,2011/competitions`;
+const fetchTable = async (code) => {
+  const url = `https://v3.football.api-sports.io/standings?league=${code}&season=2022`;
   const response = await fetch(url, requestOptions);
-  console.log(...response.headers);
   const data = await response.json();
-  console.log(data);
+  standingsList[code] = data.response[0].league;
+  localStorage.setItem('standings', JSON.stringify(standingsList));
+  addModalToDom(code, data.response[0].league.name, data.response[0].league.standings[0]);
 };
 
-// loadScorers();
+const addBtnEventListeners = () => {
+  const btns = document.querySelectorAll('.open-table');
+  btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const leagueCode = btn.dataset.bsTarget.match(/(\d+)/)[0];
+      if (localStorage.getItem('standings')) {
+        const localStanding = JSON.parse(localStorage.getItem('standings'));
+        if (leagueCode in localStanding) {
+          const currentStanding = localStanding[leagueCode];
+          addModalToDom(leagueCode, currentStanding.name, currentStanding.standings[0]);
+          console.log('local');
+        } else {
+          fetchTable(leagueCode);
+          console.log('api');
+        }
+      } else {
+        fetchTable(leagueCode);
+        console.log('api');
+      }
+    });
+  });
+};
+
+const loadLeagues = async () => {
+  await Promise.all(
+    leagueCodes.map(async (code) => {
+      const url = `https://v3.football.api-sports.io/leagues?id=${code}`;
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      addCardToDOM(data.response[0].league);
+      console.log(data.response[0]);
+      leagueList[code] = data.response[0];
+    }),
+  );
+  localStorage.setItem('leagues', JSON.stringify(leagueList));
+  addBtnEventListeners();
+};
+
+const loadLocalLeagues = () => {
+  const data = JSON.parse(localStorage.getItem('leagues'));
+  Object.keys(data).forEach((code) => {
+    addCardToDOM(data[code].league);
+  });
+  addBtnEventListeners();
+};
+
+const loadTableModalBase = () => {
+  leagueCodes.forEach((code) => {
+    const modalClone = modalTemplate.content.cloneNode(true);
+    const main = document.querySelector('main');
+    modalClone.querySelector('.modal').setAttribute('id', `league${code}`);
+    main.appendChild(modalClone);
+  });
+};
+loadTableModalBase();
 
 if (!localStorage.getItem('leagues')) {
+  console.log('api');
   loadLeagues();
 } else {
   loadLocalLeagues();
+  console.log('local');
 }
-
-// loadStandings();
